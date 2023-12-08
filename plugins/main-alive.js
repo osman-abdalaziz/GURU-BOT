7d0104f229d94eead39bcf79c7aace723369d8a7
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-08 13:12:19 +0200

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -7,30 +7,29 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     var vn = "https://cdn.jsdelivr.net/gh/Guru322/GURU-BOT@main/Assets/mp3/Alive.mp3"
     let url = "https://github.com/Guru322/GURU-BOT"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://cdn.wallpapersafari.com/71/19/7ZfcpT.png"
+    let img = "https://i.ibb.co/RpDMgWt/guru.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
-        audio: {
-          url: vn
-        },
-        mimetype: 'audio/mpeg',
-        ptt: true,
-        waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Guru",
+        // audio: {
+        //   url: vn
+        // },
+        // mimetype: 'audio/mpeg',
+        // ptt: true,
+        // waveform:  [100, 0, 100, 0, 100, 0, 100],
+        // fileName: "Guru",
     
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
           title: "I AM ALIVE",
-          body: "GURU BOT",
+          body: "KLO BAL7B BOT",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp',
+          sourceUrl: '',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
       };
-         // m.chat, doc, { quoted: con }
-      await conn.sendMessage("Yes I am Alive ^_^");
+      await conn.sendMessage(m.chat, doc, { quoted: con });
     
     }
     
