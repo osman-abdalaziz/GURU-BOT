79c8bcbfc7f3d4525f67accabd8808d0dd4a842c
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-10 18:09:30 +0200

    Update owner-addsudo.js

--- plugins/owner-addsudo.js
+++ plugins/owner-addsudo.js
@@ -9,10 +9,9 @@ let handler = async (m, { conn, text }) => {
     if (!who) throw 'Tag the person you want to make an Owner!';
     if (global.owner.includes(who.split('@')[0])) throw 'This person is already an owner!';
     global.owner.push([who.split('@')[0], name, true]);
-    const caption = `Now @${who.split('@')[0]} has been made an Owner!`;
-    await conn.reply(m.chat, caption, m, {
-        mentions: conn.parseMention(caption)
-    });
+    const caption = `🌟SUDO | Now @${who.split('@')[0]} has been made an Owner! 🥳🥳`;
+    await conn.react(🥳);
+    await conn.sendFile(m.chat, './Assets/giphy.gif', 'https://i.ibb.co/RpDMgWt/guru.jpg', caption, m, false, { mentions: [who] });
 }
 handler.help = ['addowner @user']
 handler.tags = ['owner']
