46f9eb509b03203711ca3cdfd460ba5504aaab5d
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-14 23:30:05 +0200

    Update sticker-smaker.js

--- plugins/sticker-smaker.js
+++ plugins/sticker-smaker.js
@@ -29,6 +29,7 @@ conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
 } catch (e) {
 m.reply('Conversion to sticker error, send as image instead')
 await conn.sendFile(m.chat, apiUrl, 'smaker.png', null, m)
+  conn.reply("اي خدمة يباشا")
 }}
 handler.help = ['smaker']
 handler.tags = ['sticker']
