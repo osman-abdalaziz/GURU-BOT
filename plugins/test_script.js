df0439c6e92ff4693bdc3f6a6fad37a381b81ebc
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-18 20:27:31 +0200

    Update test_script.js

--- plugins/test_script.js
+++ plugins/test_script.js
@@ -1,7 +1,21 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    m.reply(
-        "Hello, this is an experimental script that will be developed by Dr.Osman later !"
-    );
+    // m.reply(
+    //     "Hello, this is an experimental script that will be developed by Dr.Osman later !"
+    // );
+    const buttons = [
+  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1},
+  {buttonId: 'id3', buttonText: {displayText: 'Button 3'}, type: 1}
+]
+
+const buttonMessage = {
+    text: "Hi it's button message",
+    footer: 'Testing Script',
+    buttons: buttons,
+    headerType: 1
+}
+
+const sendMsg = await conn.sendMessage(id, buttonMessage)
     m.react("🧑‍💻");
 };
 
