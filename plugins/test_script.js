1fec50b670c04b085989d1bc688d370c532396ae
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-19 15:33:31 +0200

    Update test_script.js

--- plugins/test_script.js
+++ plugins/test_script.js
@@ -7,10 +7,21 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     //     "Hello, this is an experimental script that will be developed by Dr.Osman later !"
     // );
     let who;
+    if (m.isGroup)
+        who = m.mentionedJid[0]
+            ? m.mentionedJid[0]
+            : m.quoted
+            ? m.quoted.sender
+            : false;
+    else who = m.chat;
+    if (!who)
+        throw `✳️ Tag or mention someone\n\n📌 Example : ${
+            usedPrefix + command
+        } @user`;
     let name = conn.getName(m.sender);
     // Format the repository information with emojis
     const formattedInfo = `
-👤 Member Name: ${who}
+👤 Member Name: @${who.split("@")[0]}
 ⭐ Admin: ${name}
 📝 Description: ${text}`.trim();
 
@@ -19,8 +30,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         m.chat,
         {
             requestPaymentMessage: {
-                currencyCodeIso4217: "EGP",
-                amount1000: 10000,
+                currencyCodeIso4217: "USD",
+                amount1000: 5000,
                 requestFrom: m.sender,
                 noteMessage: {
                     extendedTextMessage: {
@@ -38,10 +49,45 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         {}
     );
     m.react("🧑‍💻");
+
+    //send a template message!
+    const templateButtons = [
+        {
+            index: 1,
+            urlButton: {
+                displayText: "⭐ Star Baileys on GitHub!",
+                url: "https://github.com/adiwajshing/Baileys",
+            },
+        },
+        {
+            index: 2,
+            callButton: {
+                displayText: "Call me!",
+                phoneNumber: "+1 (234) 5678-901",
+            },
+        },
+        {
+            index: 3,
+            quickReplyButton: {
+                displayText: "This is a reply, just like normal buttons!",
+                id: "id-like-buttons-message",
+            },
+        },
+    ];
+
+    const templateMessage = {
+        text: "Hi it's a template message",
+        footer: "Hello World",
+        templateButtons: templateButtons,
+    };
+
+    const sendMsg = await conn.sendMessage(m.chat, templateMessage);
 };
 
-handler.help = ["Osman"];
-handler.tags = ["Osman"];
-handler.command = /^(Osman)$/i;
+handler.help = ["penalty"];
+handler.tags = ["penalty"];
+handler.command = /^(penalty)$/i;
+handler.admin = true;
+handler.group = true;
 
 export default handler;
