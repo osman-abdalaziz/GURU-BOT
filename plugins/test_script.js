15b02b4472e92813c619214ca82c433b415d45e6
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-24 20:31:14 +0200

    Update test_script.js

--- plugins/test_script.js
+++ plugins/test_script.js
@@ -1,24 +1,21 @@
-const make = require("mumaker");
+let mumaker = require("mumaker");
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     // m.reply(
     //     "Hello, this is an experimental script that will be developed by Dr.Osman later !"
     // );
-    make.textpro(
-        "https://textpro.me/create-light-glow-sliced-text-effect-online-1068.html",
-        ["osman"]
-    )
-        .then((data) => conn.sendFile(m.chat, { image: { url: data } }))
-        .catch((err) => m.reply(err));
     m.react("✅");
+    mumaker
+        .textpro(
+            "https://textpro.me/create-light-glow-sliced-text-effect-online-1068.html",
+            ["osman"]
+        )
+        .then((data) => m.reply(data.image))
+        .catch((err) => m.reply(err));
 };
 
-handler.help = ["mlogo"];
-handler.tags = ["mlogo"];
-handler.command = /^(mlogo)$/i;
-handler.admin = true;
-handler.group = true;
+handler.help = ["makelogo"];
+handler.tags = ["makelogo"];
+handler.command = /^(makelogo)$/i;
 
 export default handler;
-
-/* 
