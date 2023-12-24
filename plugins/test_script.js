b1b4435e5e232f79cc818fb04d2335d2f03544cb
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-24 19:59:48 +0200

    Update test_script.js

--- plugins/test_script.js
+++ plugins/test_script.js
@@ -1,62 +1,24 @@
-import { promises } from "fs";
-import { join } from "path";
-import axios from "axios";
+const make = require("mumaker");
 
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
     // m.reply(
-    //     `Hello, this is an experimental script that will be developed by ${args[1]} later !`
+    //     "Hello, this is an experimental script that will be developed by Dr.Osman later !"
     // );
-//     let who;
-//     if (m.isGroup)
-//         who = m.mentionedJid[0]
-//             ? m.mentionedJid[0]
-//             : m.quoted
-//             ? m.quoted.sender
-//             : false;
-//     else who = m.chat;
-//     if (!who)
-//         throw `✳️ Tag or mention someone\n\n📌 Example : ${
-//             usedPrefix + command
-//         } @user`;
-//     let name = conn.getName(m.sender);
-//     // Format the repository information with emojis
-//     const formattedInfo = `
-// 👤 Member Name: @${who.split`@`[0]}
-
-// 📝 Description: ${text}
-
-// ⭐ Admin: ${name}`;
-
-//     // Send the formatted information as a message
-//     await conn.relayMessage(
-//         m.chat,
-//         {
-//             requestPaymentMessage: {
-//                 currencyCodeIso4217: "USD",
-//                 amount1000: 5000,
-//                 requestFrom: m.sender,
-//                 noteMessage: {
-//                     extendedTextMessage: {
-//                         text: formattedInfo,
-//                         contextInfo: {
-//                             externalAdReply: {
-//                                 showAdAttribution: true,
-//                             },
-//                         },
-//                     },
-//                 },
-//             },
-//             mentions: [who],
-//         },
-//         {}
-//     );
-//     m.react("✒️");
+    make.textpro(
+        "https://textpro.me/create-light-glow-sliced-text-effect-online-1068.html",
+        ["osman"]
+    )
+        .then((data) => conn.sendFile(m.chat, { image: { url: data } }))
+        .catch((err) => m.reply(err));
+    m.react("✅");
 };
 
-handler.help = ["penalty"];
-handler.tags = ["penalty"];
-handler.command = /^(penalty)$/i;
+handler.help = ["mlogo"];
+handler.tags = ["mlogo"];
+handler.command = /^(mlogo)$/i;
 handler.admin = true;
 handler.group = true;
 
 export default handler;
+
+/* 
