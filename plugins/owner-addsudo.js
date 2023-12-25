f6f634b0d5b0a08107df5691f22b33dab7eecea0
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-25 10:56:14 +0200

    Update owner-addsudo.js

--- plugins/owner-addsudo.js
+++ plugins/owner-addsudo.js
@@ -1,44 +1,22 @@
 let handler = async (m, { conn, text }) => {
     let who;
     if (m.isGroup) {
-        who = m.mentionedJid[0]
-            ? m.mentionedJid[0]
-            : m.quoted
-            ? m.quoted.sender
-            : text;
+        who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
     } else {
         who = m.chat;
     }
-    if (!who) throw "Tag the person you want to make an Premium!";
-    let name = await conn.getName(m.quoted.sender);
-    let user = global.db.data.users[who];
-    if (global.owner.includes(who.split("@")[0]))
-        throw "This person is already an owner!";
-    global.owner.push([who.split("@")[0], name, true]);
-    m.react("✅");
-    // const caption = `🔓 Premium | Now @${
-    //     who.split("@")[0]
-    // } \n has been made a Premium user! 🥳`;
-    // conn.sendFile(m.chat, "./Assets/congrads.gif", caption, {
-    //     mentions: [who],
-    // });
-    conn.reply(
-        m.chat,
-        `
-🔓 Premium | @${who.split`@`[0]}
-now you become a Premium user! 🥳
-┌───────────
-▢ *Number:* ${user.name}
-└───────────
-`,
-        m,
-        { mentions: [who] }
-    );
-};
-handler.help = ["addsudo", "givesudo"];
-handler.tags = ["owner"];
-handler.command = /^(add|give|-)(sudo)$/i;
-handler.rowner = true;
-handler.admin = true;
+    let name = await conn.getName(m.quoted.sender)
+    if (!who) throw 'Tag the person you want to make an Owner!';
+    if (global.owner.includes(who.split('@')[0])) throw 'This person is already an owner!';
+    global.owner.push([who.split('@')[0], name, true]);
+    const caption = `Now @${who.split('@')[0]} has been made an Owner!`;
+    await conn.reply(m.chat, caption, m, {
+        mentions: conn.parseMention(caption)
+    });
+}
+handler.help = ['addowner @user']
+handler.tags = ['owner']
+handler.command = /^(add|give|-)(owner|sudo)$/i;
+handler.owner = true
 
 export default handler;
