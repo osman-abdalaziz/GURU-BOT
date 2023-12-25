30cd6615a2cbd32fd08a6aed943cfa86975b2a7a
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-25 13:27:14 +0200

    Update anime-reactions.js

--- plugins/anime-reactions.js
+++ plugins/anime-reactions.js
@@ -1,46 +1,6 @@
-import fetch from 'node-fetch';
-import GIFBufferToVideoBuffer from '../lib/Gifbuffer.js';
-
-const getBuffer = async (url) => {
-  try {
-    const response = await fetch(url);
-    const buffer = await response.arrayBuffer();
-    return Buffer.from(buffer);
-  } catch (error) {
-    console.error("Failed to get buffer", error);
-    throw new Error("Failed to get buffer");
-  }
-}
-
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  let who;
-  if (m.isGroup) {
-    who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
-  } else {
-    who = m.chat;
-  }
-
-  if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @tag`;
-
-  let name = conn.getName(who);
-  let name2 = conn.getName(m.sender);
-  m.react(rwait);
-
-  let reaction = await fetch(`https://api.waifu.pics/sfw/${command}`);
-  if (!reaction.ok) throw await reaction.text();
-  
-  let json = await reaction.json();
-  let { url } = json;
-  const gifBuffer = await getBuffer(url);
-  const gifToVideoBuffer = await GIFBufferToVideoBuffer(gifBuffer);
-
-  conn.sendMessage(
-    m.chat,
-    { video: gifToVideoBuffer, caption: `(${name2}) ${command} ${name}`, gifPlayback: true, gifAttribution: 0 },
-    { quoted: m }
-  );
-
-  m.react('☺️'); 
+  m.reply("خلاص بطل قلة ادب 😑")
+  m.react('😑'); 
 }
 
 handler.tags = ['reaction'];
