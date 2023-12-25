238028303540182aa7b07f24a545b1680ad74214
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-25 23:00:06 +0200

    Update maker-logo.js

--- plugins/maker-logo.js
+++ plugins/maker-logo.js
@@ -3,34 +3,9 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
     // m.reply(
     //     "Hello, this is an experimental script that will be developed by Dr.Osman later !"
     // );
-
-    if (text.toLowerCase() == "list") {
-        m.reply(`
-*Logo Types List*:
-
-» sliced
-» batman
-» thunder
-» magma
-» glitch
-» demon
-» frozen
-» ice
-» typography
-» foggy
-» stone
-» bear
-» forest
-» burger
-» dragon
-» pokemon
-» natural
-» shadow`);
-        m.react("📃");
-    }
     if (!text) {
         m.reply(
-            `📜 To use this command: \n\n ${usedPrefix}makelogo type Your_Text \n\n *Example:* ${usedPrefix}makelogo batman One_Two \n\n *Note:* Use "_" to separate words`
+            `📜 To use this command: \n\n ${usedPrefix}makelogo type Your_Text \n\n 📌 *Example:* ${usedPrefix}makelogo batman One_Two \n\n ❗ *Note:* Use "_" to separate words`
         );
         m.react("❎");
     }
@@ -53,112 +28,182 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
         "pokemon",
         "natural",
         "shadow",
+        "flowers",
+        "wet_glass",
+        "hacker",
     ];
+    let newArray = types.map((element) => `» ${element}`);
+    let message = `${newArray.join("\n")}`;
+    if (text.toLowerCase() == "list") {
+        m.reply(`
+*Logo Types List*:
+
+${message}`);
+        m.react("📃");
+    }
     if (args.length > 1) {
         let type = args[0].toLowerCase();
         let logoText = args[1].replace("_", " ");
 
         m.react("🕛");
         if (types.includes(type)) {
-            let txtProLink;
+            let libLink;
+            let library;
             switch (type) {
                 case "sliced":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-light-glow-sliced-text-effect-online-1068.html";
                     break;
                 case "batman":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/make-a-batman-logo-online-free-1066.html";
                     break;
                 case "thunder":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-thunder-text-effect-online-881.html";
                     break;
                 case "magma":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-a-magma-hot-text-effect-online-1030.html";
                     break;
                 case "glitch":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-impressive-glitch-text-effects-online-1027.html";
                     break;
                 case "demon":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-green-horror-style-text-effect-online-1036.html";
                     break;
                 case "frozen":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-realistic-3d-text-effect-frozen-winter-1099.html";
                     break;
                 case "ice":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/ice-cold-text-effect-862.html";
                     break;
                 case "typography":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-artistic-typography-online-1086.html";
                     break;
                 case "foggy":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/write-text-on-foggy-window-online-free-1015.html";
                     break;
                 case "stone":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/3d-stone-cracked-cool-text-effect-1029.html";
                     break;
                 case "bear":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/online-black-and-white-bear-mascot-logo-creation-1012.html";
                     break;
                 case "forest":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/double-exposure-text-effect-black-white-976.html";
                     break;
                 case "burger":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-burger-3d-text-effect-1111.html";
                     break;
                 case "dragon":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-3d-dragon-scale-text-effect-online-1127.html";
                     break;
                 case "pokemon":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-pokemon-logo-style-text-effect-online-1134.html";
                     break;
                 case "natural":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/natural-leaves-text-effect-931.html";
                     break;
                 case "shadow":
-                    txtProLink =
+                    library = "textpro";
+                    libLink =
                         "https://textpro.me/create-a-gradient-text-shadow-effect-online-1141.html";
                     break;
+                case "flowers":
+                    library = "photooxy";
+                    libLink =
+                        "https://photooxy.com/logo-and-text-effects/make-nature-3d-text-effects-364.html";
+                    break;
+                case "wet_glass":
+                    library = "ephoto";
+                    libLink =
+                        "https://en.ephoto360.com/write-text-on-wet-glass-online-589.html";
+                    break;
+                case "hacker":
+                    library = "ephoto";
+                    libLink =
+                        "https://en.ephoto360.com/create-anonymous-hacker-avatars-cyan-neon-677.html";
+                    break;
             }
-            let res = await mumaker
-                .textpro(txtProLink, [`${logoText}`])
-                .then((data) =>
-                    conn.sendMessage(m.chat, {
-                        image: { url: `${data.image}` },
-                        caption: "✅ Done this is your logo",
-                    })
-                )
-                .catch((err) => m.reply(err));
-            m.react("✅");
-        } else if (type == "flowers") {
-            let res = await mumaker
-                .photooxy(
-                    "https://photooxy.com/logo-and-text-effects/make-nature-3d-text-effects-364.html",
-                    [`${logoText}`]
-                )
-                .then((data) =>
-                    conn.sendMessage(m.chat, {
-                        image: { url: `${data.image}` },
-                        caption: "✅ Done this is your logo",
-                    })
-                )
-                .catch((err) => m.reply(err));
-            m.react("✅");
+            switch (library) {
+                case "textpro":
+                    let res = await mumaker
+                        .textpro(libLink, [`${logoText}`])
+                        .then((data) =>
+                            conn.sendMessage(m.chat, {
+                                image: { url: `${data.image}` },
+                                caption: "✅ Done this is your logo",
+                            })
+                        )
+                        .catch((err) => m.reply(err));
+                    m.react("✅");
+                    break;
+                case "photooxy":
+                    let resS = await mumaker
+                        .photooxy(libLink, [`${logoText}`])
+                        .then((data) =>
+                            conn.sendMessage(m.chat, {
+                                image: { url: `${data.image}` },
+                                caption: "✅ Done this is your logo",
+                            })
+                        )
+                        .catch((err) => m.reply(err));
+                    m.react("✅");
+                    break;
+                case "ephoto":
+                    let resSs = await mumaker
+                        .ephoto(libLink, [`${logoText}`])
+                        .then((data) =>
+                            conn.sendMessage(m.chat, {
+                                image: { url: `${data.image}` },
+                                caption: "✅ Done this is your logo",
+                            })
+                        )
+                        .catch((err) => m.reply(err));
+                    m.react("✅");
+                    break;
+            }
+            // let res = await mumaker
+            //     .textpro(libLink, [`${logoText}`])
+            //     .then((data) =>
+            //         conn.sendMessage(m.chat, {
+            //             image: { url: `${data.image}` },
+            //             caption: "✅ Done this is your logo",
+            //         })
+            //     )
+            //     .catch((err) => m.reply(err));
+            // m.react("✅");
         }
     }
 };
