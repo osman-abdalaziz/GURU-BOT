cc85c6a5ce4dbaecd646a1265f4c485857e456ce
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-25 11:26:51 +0200

    Update owner-addsudo.js

--- plugins/owner-addsudo.js
+++ plugins/owner-addsudo.js
@@ -1,22 +1,33 @@
 let handler = async (m, { conn, text }) => {
     let who;
     if (m.isGroup) {
-        who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
+        who = m.mentionedJid[0]
+            ? m.mentionedJid[0]
+            : m.quoted
+            ? m.quoted.sender
+            : text;
     } else {
         who = m.chat;
     }
-    let name = await conn.getName(m.quoted.sender)
-    if (!who) throw 'Tag the person you want to make an Owner!';
-    if (global.owner.includes(who.split('@')[0])) throw 'This person is already an owner!';
-    global.owner.push([who.split('@')[0], name, true]);
-    const caption = `Now @${who.split('@')[0]} has been made an Owner!`;
-    await conn.reply(m.chat, caption, m, {
-        mentions: conn.parseMention(caption)
-    });
-}
-handler.help = ['addowner @user']
-handler.tags = ['owner']
-handler.command = /^(add|give|-)(owner|sudo)$/i;
-handler.owner = true
+    let name = await conn.getName(m.quoted.sender);
+    if (!who) throw "Tag the person you want to make an Sudo!";
+    if (global.owner.includes(who.split("@")[0]))
+        throw "This person is already an Sudo!";
+    global.owner.push([who.split("@")[0], name, true]);
+    const caption = `🔓 SUDO | @${
+        who.split("@")[0]
+    } \n Now has been made an Sudo user! 🥳`;
+    await conn.sendMessage(
+        m.chat,
+        { Image: "./Assets/congrads.gif", caption: caption },
+        m,
+        { mentions: conn.parseMention(caption) }
+    );
+};
+handler.help = ["addsudo @user"];
+handler.tags = ["owner"];
+handler.command = /^(add|give|-)(sudo)$/i;
+handler.rowner = true;
+handler.admin = true;
 
 export default handler;
