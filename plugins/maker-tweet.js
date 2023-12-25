3938cebede62b94076630a25488080cf98d8563d
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-25 15:53:04 +0200

    Update maker-tweet.js

--- plugins/maker-tweet.js
+++ plugins/maker-tweet.js
@@ -1,13 +1,22 @@
 let handler = async (m, { conn, text }) => {
     if (!text) throw "No Text";
 
+    // Generate a random integer between min (inclusive) and max (exclusive)
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    var randomInteger = getRandomInt(1, 10); // Example: generates a random integer between 1 (inclusive) and 10 (exclusive)
+
     const avatar = await conn
         .profilePictureUrl(m.sender, "image")
         .catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png");
     const displayName = conn.getName(m.sender);
     const username = m.sender.split("@")[0];
-    const replies = "1203"; // Replace with the desired value
-    const retweets = "432"; // Replace with the desired value
+    const replies = `${randomInteger(400, 250)}`; // Replace with the desired value
+    const retweets = `${randomInteger(300, 450)}`; // Replace with the desired value
     const theme = "dark"; // Replace with the desired value
 
     const url = `https://some-random-api.com/canvas/misc/tweet?displayname=${encodeURIComponent(
