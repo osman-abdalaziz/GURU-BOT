0281a63cbf83e819e2fbfbda18ec4bfdc02dc5da
osman-abdalaziz <66269120+osman-abdalaziz@users.noreply.github.com>
2023-12-25 10:49:26 +0200

    Update owner-addsudo.js

--- plugins/owner-addsudo.js
+++ plugins/owner-addsudo.js
@@ -1,5 +1,6 @@
 let handler = async (m, { conn, text }) => {
     let who;
+    if (!who) throw "Tag the person you want to make an Premium!";
     if (m.isGroup) {
         who = m.mentionedJid[0]
             ? m.mentionedJid[0]
@@ -10,16 +11,29 @@ let handler = async (m, { conn, text }) => {
         who = m.chat;
     }
     let name = await conn.getName(m.quoted.sender);
-    if (!who) throw "Tag the person you want to make an Premium!";
+    let user = global.db.data.users[who];
     if (global.owner.includes(who.split("@")[0]))
         throw "This person is already an owner!";
     global.owner.push([who.split("@")[0], name, true]);
-    const caption = `🔓 Premium | Now @${
-        who.split("@")[0]
-    } \n has been made a Premium user! 🥳`;
-    conn.sendFile(m.chat, "./Assets/congrads.gif", caption, {
-        mentions: [who],
-    });
+    m.react("✅");
+    // const caption = `🔓 Premium | Now @${
+    //     who.split("@")[0]
+    // } \n has been made a Premium user! 🥳`;
+    // conn.sendFile(m.chat, "./Assets/congrads.gif", caption, {
+    //     mentions: [who],
+    // });
+    conn.reply(
+        m.chat,
+        `
+🔓 Premium | @${who.split`@`[0]}
+now you become a Premium user! 🥳
+┌───────────
+▢ *Number:* ${user.name}
+└───────────
+`,
+        m,
+        { mentions: [who] }
+    );
 };
 handler.help = ["addsudo", "givesudo"];
 handler.tags = ["owner"];
